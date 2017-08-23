f5bb7a6419f4b7e33af10b682ab644fb8d1c27f2
kenilgadhiya <30653363+kenilgadhiya@users.noreply.github.com>
2017-08-23 17:43:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,20 @@ button.oneclick = function () {
     request.open('GET', 'http://http://kenilgadhiya.imad.hasura-app.io/counter' , true);
     
 };
+
+//submit name
+var nameInput == document.getElementById('name');
+var name = nameInput.value;
+var sbmit = document.getElementById('submit_btn');
+submit.oneclick = function () {
+  // make a request to the server and send the name
+  
+  // capture a list of names and render it as a list
+  var name = ['name1','name2','name3','name4'];
+  var list = '';
+  for (var i=0;i<=names.length; i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
