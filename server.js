034611611e4f1d8b3f44091ce426479a1f9970a0
kenilgadhiya <30653363+kenilgadhiya@users.noreply.github.com>
2017-08-29 11:52:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,42 +1,38 @@
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto')
 
-var app = express();
+var config = {
+   user: 'kenilgadhiya',
+   database: 'kenilgadhiya',
+   host: 'db.imad.hasura-app.io',
+   port: '5432',
+   password: process.env.DB_PASSWORD
+};
+
+var app =express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-var counter = 0;
-app.get('/counnter', function (req, res){
-    counter=counter+1;
+function createTemplate (data){}
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articl-one',function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'articl-one.html'));
+function hash (input,salt){
+    //how dowe create a hash?
+    var hashed = crypto.pdkd25sync(input,salt, 10000, 512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req.params.input,"this some random string");
+    res.send(hashedString);
 });
 
-app.get('/article-two',function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));  
-});
-
-app.get('/article-three',function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));  
-});
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    //make a select request
+})
